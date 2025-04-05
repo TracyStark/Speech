83333c836b7d988f9bdf4d21dd2a3010ee4dc4b6
TracyStark <zhouxy0814@icloud.com>
2025-04-05 12:18:20 +0800

    Initial Commit

---
+++ SpeechAssistantTests/SpeechAssistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechAssistantTests.swift
+//  SpeechAssistantTests
+//
+//  Created by Xiaoyang Zhou on 4/5/25.
+//
+
+import Testing
+@testable import SpeechAssistant
+
+struct SpeechAssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
